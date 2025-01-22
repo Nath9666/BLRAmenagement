43519c47bea13de031155e9891121299f0531dae
Nath9666 <nathan.morel@efrei.net>
2025-01-22 17:46:56 +0100

    add heropiece in asset

--- move.py
+++ move.py
@@ -5,6 +5,26 @@ if __name__ == '__main__':
     pathTest = "./assets/vrac"
     PathLego = "./assets/Lego"
 
-    #list all files in the directory
+    # List all directories in PathLego
+    dirInLego = os.listdir(PathLego)
+
+    for dir in dirInLego:
+        dirPath = os.path.join(PathLego, dir)
+        # Check if the directory is empty
+        if os.path.isdir(dirPath) and not os.listdir(dirPath):
+            os.rmdir(dirPath)
+
+    # List all files in the directory
     files = os.listdir(pathTest)
     print(files)
+
+    for file in files:
+        tempFile = file
+        pathTempFile = os.path.join(pathTest, tempFile)
+        pathFinalTempFile = os.path.join(PathLego, tempFile.split(".")[0])
+
+        if not os.path.exists(pathFinalTempFile):
+            os.makedirs(pathFinalTempFile)
+            os.rename(pathTempFile, os.path.join(pathFinalTempFile, tempFile))
+        else:
+            print("The directory already exists", pathFinalTempFile)
